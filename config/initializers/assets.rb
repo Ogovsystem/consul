8b3b3b503b4329a8c047ebc2eeae130157046134
Gimena <gimena.pujol@ogovsystem.com>
2021-03-10 15:42:24 +0100

    alinear con el estilo del visor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,5 +18,12 @@ Rails.application.config.assets.precompile += %w[sdg/*.png]
 Rails.application.config.assets.precompile += %w[sdg/**/*.png]
 Rails.application.config.assets.precompile += %w[custom/modernizr.js]
 Rails.application.config.assets.precompile += %w[custom/pace.min.js]
+Rails.application.config.assets.precompile += %w[custom/jquery-2.1.3.min.js]
+Rails.application.config.assets.precompile += %w[custom/plugins.js]
+Rails.application.config.assets.precompile += %w[custom/main.js]
 
 # Loads custom images and custom fonts before app/assets/images and app/assets/fonts
+
+Rails.application.config.assets.precompile += %w[custom/muli/muli-bold-webfont.woff2]
+Rails.application.config.assets.precompile += %w[custom/logo_200px.png]
+Rails.application.config.assets.precompile += %w[custom/5ecc29b1678908436cff2005b5ebcf29_9a3afba398c6444430cdb27f4ed7a27c.jpg]
